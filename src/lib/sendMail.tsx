834f42f814ab77e860857f6e7c9cb64ef1cd0755
md-mahamadul-hasan-shaikat <113624647+md-mahamadul-hasan-shaikat@users.noreply.github.com>
2026-03-31 21:14:09 +0600

    send mail

--- src/lib/sendMail.tsx
+++ src/lib/sendMail.tsx
@@ -1,8 +1,18 @@
 import nodemailer from 'nodemailer'
 const transporter = nodemailer.createTransport({
-    service:'gmail',
-    auth:{
-        user:
-        pass:
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASS
     }
 })
+
+export const sendEmail = async (to: string, subject: string, html: string) => {
+    await transporter.sendMail({
+        from: `"RYDEX" <${process.env.EMAIL}>`,
+        to,
+        subject,
+        html
+    })
+
+}
