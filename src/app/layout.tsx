ec64e1b35dc34ba72740276d459b8417ca973dbc
md-mahamadul-hasan-shaikat <113624647+md-mahamadul-hasan-shaikat@users.noreply.github.com>
2026-04-02 00:32:41 +0600

    Vehicle_Booking_Website-Next.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Provider from "@/lib/Provider";
+import ReduxProvider from "@/redux/ReduxProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,13 +25,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">
+    <html lang="en" >
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+
+      >
         <Provider>
-          {children}
+          <ReduxProvider>
+            {children}
+          </ReduxProvider>
         </Provider>
 
       </body>
